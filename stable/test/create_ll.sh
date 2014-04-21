749855c2ca3412fb8fb5a449cc3bed14643303a9
Nick Lewycky <nicholas@mxc.ca>
2014-04-21 22:43:46 +0000

    Cleaning up stable branch
    git-svn-id: https://llvm.org/svn/llvm-project/polly/branches/google@206808 91177308-0d34-0410-b5e6-96231b3b80d8

--- stable/test/create_ll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LLFILE=`echo $1 | sed -e 's/\.c/.ll/g'`
-
-clang -c -S -emit-llvm -O0 $1 -o ${LLFILE}
-
-opt -correlated-propagation -mem2reg -instcombine -loop-simplify -indvars \
--instnamer ${LLFILE} -S -o ${LLFILE}
