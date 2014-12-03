86a2cb423a88a538df158095750164073ba532d3
Nick Lewycky <nicholas@mxc.ca>
2014-12-03 22:43:45 +0000

    Updating branches/google/testing to r220284
    git-svn-id: https://llvm.org/svn/llvm-project/polly/branches/google@223287 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ testing/include/polly/ScheduleOptimizer.h
@@ -0,0 +1,19 @@
+//===------ polly/ScheduleOptimizer.h - The Schedule Optimizer *- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef POLLY_SCHEDULE_OPTIMIZER_H
+#define POLLY_SCHEDULE_OPTIMIZER_H
+
+namespace polly {
+extern bool DisablePollyTiling;
+}
+
+#endif
